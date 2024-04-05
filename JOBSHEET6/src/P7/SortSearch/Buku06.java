afac4a4cf37412ffd9bd92b0c8bb4dac29214df9
AJULUMEL <144210127+AJULUMEL@users.noreply.github.com>
2024-04-05 23:41:18 +0700

    JOBSHEET 6 DONE

---
+++ JOBSHEET6/src/P7/SortSearch/Buku06.java
@@ -0,0 +1,26 @@
+package JOBSHEET6.src.P7.SortSearch;
+
+public class Buku06 {
+    String kodeBuku;
+    String judulBuku;
+    int tahunTerbit;
+    String pengarang;
+    int stock;
+
+    public Buku06(String kodeBuku, String judulBuku, int tahunTerbit, String pengarang, int stock) {
+    this.kodeBuku = kodeBuku;
+    this.judulBuku = judulBuku;
+    this.tahunTerbit = tahunTerbit;
+    this.pengarang = pengarang;
+    this.stock = stock;
+    }
+
+    public void tampilDataBuku() {
+        System.out.println("================================");
+        System.out.println("Kode buku :" + kodeBuku);
+        System.out.println("Judul buku :" + judulBuku);
+        System.out.println("Tahun terbit :" + tahunTerbit);
+        System.out.println("Pengarang :" + pengarang);
+        System.out.println("Stock :" + stock);
+    }
+}
