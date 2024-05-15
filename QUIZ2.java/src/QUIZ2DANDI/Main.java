2f332cbcffa5c7c336df2910ad0d7dfbfeafc5d2
AJULUMEL <144210127+AJULUMEL@users.noreply.github.com>
2024-05-15 14:22:15 +0700

    kuis 2

--- QUIZ2.java/src/QUIZ2DANDI/Main.java
+++ QUIZ2.java/src/QUIZ2DANDI/Main.java
@@ -2,62 +2,51 @@ package QUIZ2DANDI;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         DoubleLinkedList dll = new DoubleLinkedList();
         dll.addFirst(45);
         dll.addFirst(10);
         dll.addFirst(10);
         dll.addFirst(15);
         dll.addFirst(150);
+        System.out.println("Original List:");
         dll.print();
-        dll.deleteFirst();
-        dll.print();
-        dll.printFromTail();
-        // lanjutkan dengan memanggil method addLast, deleteLast, printFromTail
         dll.deleteLast();
         dll.print();
         dll.printFromTail();
-        // lanjutkan dengan memanggil method merge, split
-        DoubleLinkedList dll2 = new DoubleLinkedList();
-        dll2.addFirst(5);
-        dll2.addFirst(4);
-        dll2.addFirst(3);
-        dll2.addFirst(2);
-        dll2.addFirst(1);
-
-        dll.merge(dll2);
-        dll.print();
-        dll.printFromTail();
-
-    }
-
-    // No. 3 Kode Ganjil
-    // public static void merge(DoubleLinkedList dll1,
-    // DoubleLinkedList dll2) {
-    // complete this method
-    // }
-
-    // No. 3 Kode Genap
-    public static void split(DoubleLinkedList dll){
-        DoubleLinkedList list1 = new DoubleLinkedList();
-        DoubleLinkedList list2 = new DoubleLinkedList();
-
-        int mid = dll.size / 2;
-        Node current = dll.head;
-
-        for (int i = 0; i < mid; i++) {
-            list1.addFirst(current.data);
-            current = current.n;
+        split(dll);
+        //lanjutkan dengan memanggil method addLast, deleteLast, printFromTail
+        //lanjutkan dengan memanggil method merge, split
         }
-
-        for (int i = mid; i < dll.size; i++) {
-            list2.addFirst(current.data);
-            current = current.n;
+        // No. 3 Kode Ganjil
+        public static void merge(DoubleLinkedList dll1,
+                DoubleLinkedList dll2){
+            //complete this method
         }
-
-        System.out.print("List 1: ");
-        list1.print(); 
-        System.out.print("List 2: ");
-        list2.print(); 
+            // //No. 3 Kode Genap
+            public static void split(DoubleLinkedList dll) {
+                if (dll.isEmpty() || dll.head == dll.tail) {
+                } else {
+                    DoubleLinkedList dll2 = new DoubleLinkedList();
+                    Node half = dll.head;
+                    Node full = dll.head;
+            
+                    while (full.n!= null && full.n.n!= null) {
+                        half = half.n;
+                        full = full.n.n;
+                    }
+            
+                    dll2.head = half.n;
+                    dll2.head.p = null;
+                    half.n = null;
+                    dll2.tail = dll.tail;
+                    dll2.size = dll.size - (dll.size / 2);
+                    dll.size = dll.size / 2;
+        
+                    System.out.println("List 1:");
+                    dll.print();
+                    System.out.println("List 2:");
+                    dll2.print();
+                }
+            }
     }
-}
