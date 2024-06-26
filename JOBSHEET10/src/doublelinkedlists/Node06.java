16ad9d2b299ff40101eea9cfaea133ffaf64611e
AJULUMEL <144210127+AJULUMEL@users.noreply.github.com>
2024-06-26 21:49:53 +0700

    praktikum 2

--- JOBSHEET10/src/doublelinkedlists/Node06.java
+++ JOBSHEET10/src/doublelinkedlists/Node06.java
@@ -1,10 +1,10 @@
 package JOBSHEET10.src.doublelinkedlists;
 
-public class Node {
+public class Node06 {
     int data;
-    Node prev, next;
+    Node06 prev, next;
 
-    Node(Node prev, int data, Node next) {
+    Node06(Node06 prev, int data, Node06 next) {
         this.prev = prev;
         this.data = data;
         this.next = next;
